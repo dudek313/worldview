496798ba16743057925254f48e136f8b07015959
Kiri <kiri@intersect.org.au>
2025-04-30 17:05:51 +1000

    WIP: Replace @NAME@ with DCCEEW Worldview Beta

--- web/js/components/tour/modal-tour-complete.js
+++ web/js/components/tour/modal-tour-complete.js
@@ -51,10 +51,10 @@ function ModalComplete(props) {
         </ModalHeader>
         <ModalBody>
           <p>
-            You have now completed a story in @NAME@. To view more stories,
+            You have now completed a story in DCCEEW Worldview Beta. To view more stories,
             click the &ldquo;More Stories&rdquo; button below to explore more events
             within the app. Click the &ldquo;Exit Tutorial&rdquo; button or close this
-            window to start using @NAME@ on your own.
+            window to start using DCCEEW Worldview Beta on your own.
           </p>
           {list}
         </ModalBody>
